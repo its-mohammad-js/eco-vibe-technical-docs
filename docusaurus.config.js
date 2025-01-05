b5420908503706e494a9c469cb2e637feaf15de0
its-mohammad-js <mohammadreactjs@gmail.com>
2025-01-05 15:11:39 +0330

    deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://its-mohammad-js.github.io/eco-vibe-technical-docs/",
+  url: "/eco-vibe-technical-docs/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
